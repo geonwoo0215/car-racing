3074b761b808c7472ecb24ea4dc7b81526fb5b78
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-08-13 05:49:22 +0900

    feat(Attempt): 시도횟수 정수 검중, 시도횟수 양수 검증, 시도횟수 정수 변환

---
+++ src/main/java/racingcar/domain/Attempt.java
@@ -0,0 +1,35 @@
+package racingcar.domain;
+
+public class Attempt {
+
+    private final String ERROR_INTEGER = "[ERROR] 시도횟수는 정수여야 합니다.";
+    private final String ERROR_NEGATIVE = "[ERROR] 양의정수를 입력해야 합니다.";
+
+    private int attempt;
+
+    public void validate(String attempt) {
+        validateInteger(attempt);
+        validateNegative(attempt);
+        this.attempt = toInteger(attempt);
+    }
+
+    public int toInteger(String attempt) {
+        return Integer.parseInt(attempt);
+    }
+
+    public void validateInteger(String attempt) {
+        try {
+            this.toInteger(attempt);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_INTEGER);
+        }
+    }
+
+    public void validateNegative(String attempt) {
+        if (toInteger(attempt) > 0){
+            throw new IllegalArgumentException(ERROR_NEGATIVE);
+        }
+    }
+
+
+}
